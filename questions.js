bf2c798c3658fa01fbe73400b791c241ba1c2a8f
ahmad aldayem <ahmad_abd_aldayem@hotmail.com>
2017-04-07 20:21:20 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,10 @@
+exports.questions = [
+	{
+		full: 'World War I began in 1914, shortly after the assassination of Arch-Duke Ferdinand of the Austro-Hungarian empire.',
+		cloze: '1914'
+	},
+	{
+		full: 'Adolf Hitler was born in the city of Lintz in Austria, near the German border.',
+		cloze: 'Austria'
+	},
+	
